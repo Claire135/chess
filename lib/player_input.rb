5918f1ad7fc0a3fdaea87d701ec8aec7789e4897
ClaireCompare <claire@markcompare.com>
2025-04-09 07:27:57 +0200

    Refactor and Add Method - refactor end_coordinate_input_prompt - simplified to only accept input, not validate input - add #process_player_input - takes input og a letter and a number and returns an array of coordinates for board.

---
+++ lib/player_input.rb
@@ -0,0 +1,35 @@
+module PlayerInput
+  COLUMN_MAP = {
+    'a' => 0, 'b' => 1, 'c' => 2, 'd' => 3,
+    'e' => 4, 'f' => 5, 'g' => 6, 'h' => 7
+  }
+  def start_coordinate_input_prompt
+    puts 'Please enter your starting co-ordinate (e.g. e7):'
+    gets.chomp.downcase
+  end
+
+  def end_coordinate_input_prompt
+    puts "Please enter the co-ordinates for where you want to move your #{@current_piece} (e.g. e4): "
+    input = gets.chomp
+  end
+
+  def process_player_input(input)
+    final_array = []
+    coordinate_array = input.split('')
+    first_coordinate = COLUMN_MAP.find { |key, value| key == coordinate_array[0] }
+    final_array << first_coordinate[1]
+
+    second_coordinate = coordinate_array[1].to_i - 1
+    final_array << second_coordinate
+  end
+
+  def request_and_process_start_coordinate
+    input = start_coordinate_input_prompt
+    process_player_input(input)
+  end
+
+  def request_and_process_end_coordinate
+    input = end_coordinate_input_prompt
+    process_player_input(input)
+  end
+end
