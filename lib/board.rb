cc15497280e11576f5fcbd55b3d085f33c3ad53e
ClaireCompare <claire@markcompare.com>
2025-04-15 17:10:26 +0200

    Board Refactor - initialize board with pieces set up rather than as an empty array #set_up_board - add WHITE_PIECE_ARRAY and BLACK_PIECE_ARRAY with all pieces to declutter #initialize - add #path_clear? and helper method #between - add rspec tests for #path_clear, #between and #initialize

--- lib/board.rb
+++ lib/board.rb
@@ -6,60 +6,57 @@ require_relative 'pieces/pawn'
 require_relative 'pieces/castle'
 require_relative 'pieces/queen'
 
+# captured_piece_at - identifies captured piece
+# delete_captured_piece - deletes captured piece
+# [] - facilitate addition of coordinates from MoveContext
+# empty_at? - checks if a square is empty
+# enemy_piece_at? = checks if a pieces is of a different color
+
 class Board
   attr_reader :white_pieces, :black_pieces
   attr_accessor :board
 
   def initialize
-    @board = Array.new(8) { Array.new(8) }
-    @white_pieces = [
-      Castle.new('white', [7, 0], '♖', 'Castle 1'),
-      Castle.new('white', [7, 7], '♖', 'Castle 2'),
-      Knight.new('white', [7, 1], '♘', 'Knight 1'),
-      Knight.new('white', [7, 6], '♘', 'Knight 2'),
-      Bishop.new('white', [7, 2], '♗', 'Bishop 1'),
-      Bishop.new('white', [7, 5], '♗', 'Bishop 2'),
-      Queen.new('white', [7, 3], '♕', 'Queen'),
-      King.new('white', [7, 4], '♔', 'King')
-    ]
+    @white_pieces = Board::WHITE_PIECE_ARRAY.dup
 
     (0..7).each do |col|
-      @white_pieces << Pawn.new('white', [6, col], '♙', 'Pawn(1..8)')
+      @white_pieces << Pawn.new('white', [6, col], '♙', "Pawn #{col + 1}")
     end
 
-    @black_pieces = [
-      Castle.new('black', [0, 0], '♜', 'Castle 1'),
-      Knight.new('black', [0, 1], '♞', 'Knight 1'),
-      Bishop.new('black', [0, 2], '♝', 'Bishop 1'),
-      Queen.new('black', [0, 3], '♛', 'Queen'),
-      King.new('black', [0, 4], '♚', 'King'),
-      Knight.new('black', [0, 6], '♞', 'Knight 2'),
-      Bishop.new('black', [0, 5], '♝', 'Bishop 2'),
-      Castle.new('black', [0, 7], '♜', 'Castle 2')
-    ]
-
+    @black_pieces = Board::BLACK_PIECE_ARRAY.dup
     (0..7).each do |col|
-      @black_pieces << Pawn.new('black', [1, col], '♟', 'Pawn(1..8)')
+      @black_pieces << Pawn.new('black', [1, col], '♟', "Pawn #{col + 1}")
     end
+
+    @board = set_up_board
+  end
+
+  def set_up_board
+    board = Array.new(8) { Array.new(8) }
+    (@white_pieces + @black_pieces).each do |piece|
+      row, col = piece.position
+      board[row][col] = piece
+    end
+    board
   end
 
   def display_board
-    place_pieces
     generate_board
   end
 
-  def [](row, col)
-    @board[row][col]
-  end
+  # facilitates addition of coordinates from MoveContext class
+  #  def [](row, col)
+  #   @board[row][col]
+  # end
 
-  def []=(row, col, value)
-    @board[row][col] = value
-  end
+  # def []=(row, col, value)
+  # @board[row][col] = value
+  # end
 
-  def captured_piece_at(coordinate)
-    row, col = coordinate
-    @board[row][col]
-  end
+  #  def captured_piece_at(coordinate)
+  #    row, col = coordinate
+  #    @board[row][col]
+  #  end
 
   def delete_captured_piece(piece)
     if piece.color == 'white'
@@ -79,13 +76,33 @@ class Board
     piece && piece.color != color
   end
 
+  def path_clear?(start_coordinates, end_coordinates)
+    between(start_coordinates, end_coordinates).all? do |row, col|
+      @board[row][col].nil?
+    end
+  end
+
   private
 
-  def place_pieces
-    (@white_pieces + @black_pieces).each do |piece|
-      row, col = piece.position
-      @board[row][col] = piece
+  def between(start_coordinates, end_coordinates)
+    row1, col1 = start_coordinates
+    row2, col2 = end_coordinates
+
+    coordinates = []
+
+    row_step = (row2 - row1) <=> 0
+    col_step = (col2 - col1) <=> 0
+
+    current_row = row1 + row_step
+    current_col = col1 + col_step
+
+    while [current_row, current_col] != [row2, col2]
+      coordinates << [current_row, current_col]
+      current_row += row_step
+      current_col += col_step
     end
+
+    coordinates
   end
 
   def generate_board
@@ -99,4 +116,26 @@ class Board
     end
     puts '  a b c d e f g h'
   end
+
+  WHITE_PIECE_ARRAY = [
+    Castle.new('white', [7, 0], '♖', 'Castle 1'),
+    Castle.new('white', [7, 7], '♖', 'Castle 2'),
+    Knight.new('white', [7, 1], '♘', 'Knight 1'),
+    Knight.new('white', [7, 6], '♘', 'Knight 2'),
+    Bishop.new('white', [7, 2], '♗', 'Bishop 1'),
+    Bishop.new('white', [7, 5], '♗', 'Bishop 2'),
+    Queen.new('white', [7, 3], '♕', 'Queen'),
+    King.new('white', [7, 4], '♔', 'King')
+  ]
+
+  BLACK_PIECE_ARRAY = [
+    Castle.new('black', [0, 0], '♜', 'Castle 1'),
+    Knight.new('black', [0, 1], '♞', 'Knight 1'),
+    Bishop.new('black', [0, 2], '♝', 'Bishop 1'),
+    Queen.new('black', [0, 3], '♛', 'Queen'),
+    King.new('black', [0, 4], '♚', 'King'),
+    Knight.new('black', [0, 6], '♞', 'Knight 2'),
+    Bishop.new('black', [0, 5], '♝', 'Bishop 2'),
+    Castle.new('black', [0, 7], '♜', 'Castle 2')
+  ]
 end
---
+++ spec/board_spec.rb
@@ -0,0 +1,63 @@
+require_relative '../lib/board'
+require_relative '../lib/pieces/king'
+
+describe Board do
+  let(:board) { Board.new }
+  describe '#initialize' do
+    context 'when board is initialized' do
+      it 'creates 1 white king' do
+        expect(board.white_pieces.count { |p| p.is_a?(King) }).to eq(1)
+      end
+
+      it 'creates 8 black pawns' do
+        expect(board.black_pieces.count { |p| p.is_a?(Pawn) }).to eq(8)
+      end
+    end
+  end
+
+  # skipped as it is now a private method
+
+  describe '#between' do
+    subject(:board) { described_class.new }
+
+    context 'when moving vertically' do
+      xit 'returns coordinates of squares travelled minus the first and the last' do
+        expect(board.between([2, 1], [7, 1])).to eq([[3, 1], [4, 1], [5, 1], [6, 1]])
+      end
+    end
+
+    context 'when moving horizontally' do
+      xit 'returns coordinates of squares between two columns' do
+        expect(board.between([4, 2], [4, 6])).to eq([[4, 3], [4, 4], [4, 5]])
+      end
+    end
+
+    context 'when moving diagonally' do
+      xit 'returns coordinates of squares between two diagonals' do
+        expect(board.between([2, 2], [5, 5])).to eq([[3, 3], [4, 4]])
+      end
+    end
+  end
+
+  describe '#path_clear?' do
+    subject(:board) { described_class.new }
+
+    before do
+      board.instance_variable_set(:@board, Array.new(8) { Array.new(8, nil) })
+    end
+
+    context 'when the path is clear vertically' do
+      it 'returns true' do
+        expect(board.path_clear?([3, 1], [3, 5])).to eq(true)
+      end
+    end
+
+    context 'when a piece is in the path' do
+      it 'returns false' do
+        piece = double('Piece', color: 'black')
+        board.board[0][3] = piece # place a piece at c1 (index [0,3]) to block a1-f1
+        expect(board.path_clear?([0, 0], [0, 5])).to eq(false)
+      end
+    end
+  end
+end
